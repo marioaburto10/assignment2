5ba1319eec3ce9b26b3a478e834ddb0b409ead86
Mario <Mario>
2016-12-10 13:09:32 -0500

    fully deployed to heroku

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
 
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/portfolio', function(req, res){
+  res.sendFile(__dirname + '/portfolio.html')
+})
+
+app.get('/contact', function(req, res){
+  res.sendFile(__dirname + '/contact.html')
+})
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
