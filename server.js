a56cc93332054e68575bf2c69a5e002f16870383
Mario <Mario>
2016-12-10 11:53:41 -0500

    fixing deploying issue

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('App listening on PORT: ' + port);
 });
